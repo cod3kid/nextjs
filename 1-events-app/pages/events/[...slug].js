66a1c5d87ded5834a2f4416552fb36e490c6658a
cod3kid <djsufail@gmail.com>
2021-09-30 22:06:13 +0530

    Events App => Skeleton

---
+++ 1-events-app/pages/events/[...slug].js
@@ -0,0 +1,3 @@
+export default function FilteredEventsPage() {
+  return <div>Filtered Events</div>;
+}
---
+++ 1-events-app/pages/events/[eventId].js
@@ -0,0 +1,3 @@
+export default function EventDetailPage() {
+  return <div>Event Details Page</div>;
+}
